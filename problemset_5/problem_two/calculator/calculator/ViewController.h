2b50fd8db9ad903ef0d138b39e699b711ee4e0da
Elise Harris <eharris99@yahoo.com>
2015-08-28 18:45:46 -0400

    first for ps5

---
+++ problemset_5/problem_two/calculator/calculator/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  calculator
+//
+//  Created by Elise Harris on 8/26/15.
+//  Copyright (c) 2015 ech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *display;
+
+
+@end
+
