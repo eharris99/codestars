389c05a2152247ab0bd4d42fada5b41177a736f0
Elise Harris <eharris99@yahoo.com>
2015-09-05 00:42:07 -0400

    about and bio

---
+++ problemset_6/TravelApp/RemKoolhaasTravelApp/RemKoolhaasTravelApp/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  RemKoolhaasTravelApp
+//
+//  Created by Elise Harris on 9/5/15.
+//  Copyright (c) 2015 ech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UIWebView *webView;
+
+@end
---
+++ problemset_6/TravelApp/RemKoolhaasTravelApp/RemKoolhaasTravelApp/BioViewController.h
@@ -0,0 +1,15 @@
+//
+//  BioViewController.h
+//  RemKoolhaasTravelApp
+//
+//  Created by Elise Harris on 9/5/15.
+//  Copyright (c) 2015 ech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BioViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UITextView *bioView;
+
+@end
