0902bf48165d8c70b8ba14f7a6822919e634a4a0
Elise Harris <eharris99@yahoo.com>
2015-09-13 17:25:14 -0400

    ps7

---
+++ problemset_7/ToDoList/ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Elise Harris on 9/11/15.
+//  Copyright (c) 2015 ech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
