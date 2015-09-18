46d6a2fa5f7e062975efffea67868e1ab038d16c
Elise Harris <eharris99@yahoo.com>
2015-09-18 15:14:03 -0400

    ps8

---
+++ problemset_8/CustomTable-ch 12-17/CustomTable/Recipe.m
@@ -0,0 +1,13 @@
+//
+//  Recipe.m
+//  CustomTable
+//
+//  Created by Elise Harris on 9/18/15.
+//  Copyright (c) 2015 AppCoda. All rights reserved.
+//
+
+#import "Recipe.h"
+
+@implementation Recipe
+
+@end
