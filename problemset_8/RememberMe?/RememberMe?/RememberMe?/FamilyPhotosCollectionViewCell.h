605a095decb29b4a7559437741f91a47f2e87771
Elise Harris <eharris99@yahoo.com>
2015-09-19 13:43:20 -0400

    rm

---
+++ problemset_8/RememberMe?/RememberMe?/RememberMe?/FamilyPhotosCollectionViewCell.h
@@ -0,0 +1,16 @@
+//
+//  FamilyPhotosCollectionViewCell.h
+//  RememberMe?
+//
+//  Created by Elise Harris on 9/19/15.
+//  Copyright (c) 2015 ech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FamilyPhotosCollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *familyMemberImage;
+
+
+@end
