047c62dc54b157c4cb70b77f6e9dccd6ae371370
Elise Harris <eharris99@yahoo.com>
2015-09-25 23:17:59 -0400

    adds

--- RememberMe?/RememberMe9:25 trying crud/RememberMe?/AddQuestionViewController.m
+++ RememberMe?/RememberMe9:25 trying crud/RememberMe?/AddQuestionViewController.m
@@ -10,6 +10,8 @@
 
 
 @interface AddQuestionViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *saveButton;
 
 @end
 
@@ -25,14 +27,21 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    
+    if (sender != self.saveButton) return;
+    if (self.textField.text.length > 0) {
+        self.question = [[Question alloc] init];
+        self.question.questionName = self.textField.text;
+        self.question.completed = NO;
+    }
 }
-*/
+
 
 @end
